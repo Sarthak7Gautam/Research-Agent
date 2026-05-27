251dce2a48ed2ce4c886a83b3a39d0a5c97428dd
Sarthak7Gautam <sarthak271gautam@gmail.com>
2026-05-27 23:33:11 +0530

    main file

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from research-agent!")
+from langchain.messages import HumanMessage
+from core.Agent_Build_Compile.build_and_compile import AgentBuilder
+
+def main(content: str):
+    agent = AgentBuilder().build_agent()
+    messages = [HumanMessage(content=f"{content}")]
+    answer = agent.invoke({"messages": messages})
+    return answer
 
 
 if __name__ == "__main__":
-    main()
+    messages = main(content="Whats the best tourist destinations in Japan?")
+    for m in messages["messages"]:
+        print(m)
